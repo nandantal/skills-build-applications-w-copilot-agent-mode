d2164be597e62b56e21b0bf4167e516200d5f10b
Nandan Dutta <nandan.dutta@talentica.com>
2026-02-09 11:07:54 +0000

    Update settings and URLs to support dynamic ALLOWED_HOSTS and base URL configuration

--- octofit-tracker/backend/octofit_tracker/settings.py
+++ octofit-tracker/backend/octofit_tracker/settings.py
@@ -25,7 +25,17 @@ SECRET_KEY = 'django-insecure-#1gs5yri%w%8!*zc1$uch73^ltmc8*2@_rjmbs85uju)zr_*06
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+
+import os
+codespace_name = os.environ.get('CODESPACE_NAME', None)
+if codespace_name:
+    ALLOWED_HOSTS = [
+        f'{codespace_name}-8000.app.github.dev',
+        'localhost',
+        '127.0.0.1',
+    ]
+else:
+    ALLOWED_HOSTS = ['localhost', '127.0.0.1']
 
 
 # Application definition
